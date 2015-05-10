c3c99f940a0a4109a2cc4666a8c821af70970541
endenis <endenis@gmail.com>
2015-05-10 14:35:35 +0200

    Clojure 1.6.0 and m2 path fix

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,9 @@
+(defn local-repo-path []
+  (get (System/getenv) "M2_REPO" (str (get (System/getenv) "HOME") "/.m2/repository")))
+
 (defproject clojureapp "0.1.0-SNAPSHOT"
     :plugins [[lein-ring "0.7.5"]]
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [http-kit "2.1.12"]]
+    :dependencies [[org.clojure/clojure "1.6.0"]
+                   [http-kit "2.1.18"]]
+    :local-repo ~(local-repo-path)
     :main webapp)
